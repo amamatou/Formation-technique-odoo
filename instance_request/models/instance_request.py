55de9aff87b78add92f3db39593835cf5579f527
amamatou <haouaamamatououmar.mairou@etu.uae.ac.ma>
2023-02-17 17:26:06 +0100

    list view editable, groups, rules

--- instance_request/models/instance_request.py
+++ instance_request/models/instance_request.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 class InstanceRequest(models.Model):
      _name = "instance.request"
-     _description = "Demande d'instance"
+     _description = "Instance request"
 
      name = fields.Char(string="Designation")
      adress_ip = fields.Char(string="IP Address")
@@ -37,7 +37,23 @@ class InstanceRequest(models.Model):
      def action_progress(self):
           for record in self:
                record.state = 'in_process'
+               # pointe sur le groupe, recuperer l'id du groupe
+               user_group = self.env.ref('instance_request.group_manager')
+               print("==========> groupe:  ", user_group)
+               users = user_group.users
 
+               for user in users:
+                    print("======> name: ", user.name)
+
+               user_connected = self.env.user
+               print("==========> utilisateur connecté:  ", user_connected)
+               has_user_group = user_connected.has_group('instance_request.group_manager')
+               print("==========> Il appartient au groupe manager?  ", has_user_group)
+
+               model_access = user_group.model_access
+               print('============== model access =================')
+               for access in model_access:
+                    print("======> ", access.name, access.perm_create, access.perm_write, access.perm_read, access.perm_unlink)
      def action_done(self):
           for record in self:
                record.state = 'done'
